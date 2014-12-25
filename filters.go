bf6eafc02f06a933f0033df1385823ea5d42b92f
gernest <geofreyernest@live.com>
2014-12-25 20:55:52 +0300

    Added Tarehe filter

--- filters.go
+++ filters.go
@@ -1,5 +1,7 @@
 package jesus
 
+import "regexp"
+
 func Imethibitishwa(node string) (string, string) {
 	k, v := "", ""
 	if node == "Imethibitishwa" {
@@ -28,4 +30,16 @@ func MamaMia(m string) (string, string) {
 		return k, v
 	}
 	return k, v
+}
+
+func Tarehe(node string) (string, string) {
+	k, v := "", ""
+	var re = regexp.MustCompile(`^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(1[1-9])$`)
+	if re.MatchString(node) {
+		k = "Tarehe"
+		v = node
+		return k, v
+	}
+	return k, v
+
 }
